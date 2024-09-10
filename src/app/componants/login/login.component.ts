b60cebbbc9df6ca685c7c213419e250b558daf23
Mehul Sahu <mehulyashams@gmail.com>
2024-09-11 01:08:06 +0530

    ft(login and signup added with err handling in fronted)

--- src/app/componants/login/login.component.ts
+++ src/app/componants/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms"
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    email = new FormControl("",
+      [Validators.required, Validators.email]
+    )
 
+    password = new FormControl("",[
+      Validators.required, Validators.minLength(6)
+    ])
+
+    loginForm = new FormGroup({
+      email : this.email,
+      password: this.password
+    })
+
+    loginBtn(){
+      console.log(this.loginForm.value);
+      
+    }
+
+    reset(){
+      this.loginForm.reset()
+    }
 }
